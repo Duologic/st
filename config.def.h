51e71b6d1b1f323c15b5737bca1359dfed343cb6
Duologic <jeroen.opteynde@mobilevikings.com>
2015-05-08 11:32:47 +0200

    change to powerline font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Liberation Mono:pixelsize=8:antialias=true:autohint=false";
+static char font[] = "Terminess Powerline:pixelsize=8:antialias=true:autohint=false";
 static int borderpx = 1;
 static char shell[] = "/bin/sh";
 static char *utmp = NULL;
